7e1d859d5edae5c41c067f639b16ae81fb13d6af
kusung25 <kusung25@40af2166-402a-0410-87ae-1d88bcb22c19>
2007-02-24 23:48:36 +0000

    최초등록

---
+++ trunk/source/codegen/caseTool/sqlGen/script/Klass.py
@@ -0,0 +1,177 @@
+﻿# -*- coding: utf-8 -*-
+
+from CommonUtil import *
+import Field
+from sqlHelper import *
+# start
+SPACE2='  '
+SPACE4='    '
+INDENT_SPC='    '
+COLUMN_PAD_LEN=25
+aCommonUtil = CommonUtil()
+class Klass:
+	def __init__(self):
+		self.fieldList = []
+		self.operationList=[]
+		self.pkColumnList=[]
+		self.nonPkColumnList=[]		
+
+	def setAttributes(self, entityName,tableViewName):
+		self.klassEng = tableViewName
+		self.tableEng = tableViewName
+		self.klassKor = entityName
+		self.tableKor = entityName
+		
+		#self.javaClassName = entityName
+		self.name=self.klassEng
+
+		self.javaClassName = aCommonUtil.getClassName(self.tableEng)
+		
+		self.writer			=" "
+		self.writeDate		=" "
+		self.subSystemName	=" "
+
+	def addFieldList(self, aField):
+		self.fieldList.append(aField)
+		self.setpkColumnList(aField)
+		
+	def setpkColumnList(self, aField):
+		if aField.columnIsPK == 'Yes':
+			self.pkColumnList.append(aField)			
+# 			print 'aField: [',aField.name,']'
+		else:
+			self.nonPkColumnList.append(aField)
+# 			print 'aField: [',aField.name,']'			
+
+	def getpkColumnList(self):
+		return self.pkColumnList
+		
+	def addOperationList(self, aOperation):
+		self.operationList.append(aOperation)
+
+	def setClassDoc(self, aClassDoc):
+		self.classDoc = aClassDoc
+	def getClassDoc(self):
+		try:
+			out = self.classDoc
+			return out
+		except AttributeError:
+			#ClassDoc 객체가 만들어지지 않았다면 ''을 리턴한다.
+			aClassDoc=Field.ClassDoc()
+			aClassDoc.setAttributes('','')
+			return aClassDoc
+	
+	def setWriter(self, writer):
+		self.writer = writer
+	def setWriteDate(self, writeDate):
+		self.writeDate = writeDate
+	def setSubSystemName(self, subSystemName):
+		self.subSystemName = subSystemName
+
+	#---------------------------------------------------------------------------
+	def getSqlWhere(self):
+		s = ''
+		pkColumnList = self.pkColumnList
+		first = True
+		for pkCol in self.pkColumnList:
+			ce = pkCol.columnEng
+			if first :
+				s = s + ce.ljust(COLUMN_PAD_LEN,' ') +  ' = #' + pkCol.javaName + '# '
+				first=False
+			else:
+				s = s + '\n'+ SPACE2 +'AND ' + ce.ljust(COLUMN_PAD_LEN,' ') + \
+				  ' = #' + pkCol.javaName + '# '
+		return s
+	
+	def getSqlWhereTbAlias(self):
+		s = ''
+		pkColumnList = self.pkColumnList
+		first = True
+		for pkCol in self.pkColumnList:
+			ce = pkCol.columnEng
+			if first :
+				s = s + 'T1.'+ce.ljust(COLUMN_PAD_LEN,' ') +  ' = #' + pkCol.javaName + '# '
+				first=False
+			else:
+				s = s + '\n'+ SPACE2 +'AND ' + 'T1.'+ce.ljust(COLUMN_PAD_LEN,' ') + \
+				  ' = #' + pkCol.javaName + '# '
+		return s
+	
+	def setInsertSql(self, insertSql):
+		self.insertSql = insertSql
+	def getInsertSql(self):
+		return self.insertSql
+
+	def setUpdateSql(self, updateSql):
+		self.updateSql = updateSql
+	def getUpdateSql(self):
+		return self.updateSql
+
+	def setDeleteSql(self, deleteSql):
+		self.deleteSql = deleteSql
+	def getDeleteSql(self):
+		return self.deleteSql
+
+	def setSelectSql(self, selectSql):
+		self.selectSql = selectSql
+	def getSelectSql(self):
+		return self.selectSql
+
+	def setSelectSqlTbAlias(self, selectSqlTbAlias):
+		self.selectSqlTbAlias = selectSqlTbAlias
+	def getSelectSqlTbAlias(self):
+		return self.selectSqlTbAlias
+	
+	def setInsertSqlAction(self, aTable):
+
+		columnArg = array2comma(aTable)
+		rValueArg = getArray2ValueArg(aTable)
+		sql = "INSERT INTO " + aTable.tableEng +" (\n" + columnArg + ") " + \
+			  "\n" + "VALUES (\n" + rValueArg + ")"
+		aTable.setInsertSql(sql)
+		
+
+	def setUpdateSqlAction(self, aTable):
+		# UPDATE CD
+		# SET      CD_ID = #cdId:VARCHAR# 
+			 # , UPDATE_DATE = sysdate 
+		# WHERE  CD_ID = #cdId:VARCHAR#
+		# AND    CD_TP_ID = #cdTpId:VARCHAR#
+		updateColumnArg = getUpdateSetArg(aTable)
+		sqlWhere = self.getSqlWhere()
+		sql = "UPDATE " + aTable.tableEng + "\nSET\n"  + updateColumnArg \
+			+ "" + "\nWHERE " + sqlWhere
+		aTable.setUpdateSql(sql)
+		
+	def setDeleteSqlAction(self, aTable):
+		# DELETE FROM CD 
+		# WHERE  CD_ID= #cdId:VARCHAR# 
+		# AND    CD_TP_ID = #cdTpId:VARCHAR#
+		sqlWhere = aTable.getSqlWhere()
+		sql = "DELETE FROM " + aTable.tableEng  \
+			+ "\nWHERE " + sqlWhere
+		aTable.setDeleteSql(sql)
+		
+	def setSelectSqlAction(self, aTable):	
+		# SELECT CD_ID, CD_NM, CD_TP_ID, DEL_FG, to_char(UPDATE_DATE, 'YYYY/MM/DD HH24:MI:SS') UPDATE_DATE FROM CD 
+		# WHERE CD_TP_ID=#cdTpId# and CD_ID = #cdId#
+		columnArg = array2comma(aTable)
+		sqlWhere = self.getSqlWhere()
+		sql = "SELECT\n" + columnArg + \
+			  "\nFROM " + aTable.tableEng  \
+			+ "\nWHERE " + sqlWhere
+		aTable.setSelectSql(sql)
+
+	def setSelectSqlTbAliasAction(self, aTable):
+		# SELECT CD_ID, CD_NM, CD_TP_ID, DEL_FG, to_char(UPDATE_DATE, 'YYYY/MM/DD HH24:MI:SS') UPDATE_DATE FROM CD
+		# WHERE CD_TP_ID=#cdTpId# and CD_ID = #cdId#
+		columnArg = array2CommaForTbAlias(aTable)
+		sqlWhere = self.getSqlWhereTbAlias()
+		sql = "SELECT\n" + columnArg + \
+			  "\nFROM "  + aTable.tableEng +' T1' \
+			+ "\nWHERE " + sqlWhere
+		aTable.setSelectSqlTbAlias(sql)
+		
+
+
+		
---
+++ trunk/source/codegen/caseTool/sqlGen/script/SqlGenMain.py
@@ -0,0 +1,79 @@
+﻿# -*- coding: utf-8 -*-
+# 
+# generates xml files from Excel
+# Programming by KyungUk, Sung
+#
+# Copyright (c) 2002 KyungUk, Sung
+# start
+import sys
+import logging
+import utils
+# import shutil
+
+import ExcelHelperClassList
+from KlassInfoList import *
+from ExcelReader import *
+from Constants import *
+from CommonUtil import *
+import ReaderAppEnv
+
+CONS = Constants()
+
+class SqlWriterApp:  
+	def writeSqlCore(self,aReaderAppEnv):
+
+		inFile = CONS.INPUT_DATA_DIR / sys.argv[1]
+		aExcelReader = ExcelReader(inFile, CONS)
+		self.aKlassInfoList = KlassInfoList()
+
+		xx = aExcelReader.getKlassListFromExcel()
+		self.aKlassInfoList.setKlassList(xx)
+		self.aKlassInfoList.setReaderAppEnv(aReaderAppEnv)
+
+		aCommonUtil = CommonUtil()
+		for aKlass in self.aKlassInfoList.getKlassList():
+			aKlass.setInsertSqlAction(aKlass)
+			aKlass.setUpdateSqlAction(aKlass)
+			aKlass.setDeleteSqlAction(aKlass)
+			aKlass.setSelectSqlAction(aKlass)
+			aKlass.setSelectSqlTbAliasAction(aKlass)
+##			print 'aKlass.getInsertSql\n', aKlass.getUpdateSql()
+##			print 'aKlass.getInsertSql\n', aKlass.getInsertSql()
+			
+ 			outSource = aCommonUtil.generateCode(aKlass, str(CONS.SQL_TEMPLATE))
+ 			fileName = CONS.OUT_DIR / aKlass.name + '.txt'
+ 			aCommonUtil.writeFile(fileName, outSource)
+			
+		log.info('---def writeSqlCore---')
+		log.info("(MSG) Ok: write Sql")
+		print "(MSG)write Sql statement: Ok"
+
+
+if __name__ == '__main__':
+ 	if len(sys.argv) < 2:
+ 		print "USAGE: SqlWriterApp.py input.xls"
+ 		sys.exit()
+
+	inPath = sys.argv[0]
+	CONS.setConstant(inPath)
+
+	utils.initLog(CONS.LOG_FILE)
+	utils.addConsoleLogging()
+	log = logging.getLogger('ExcelMain')
+
+	#log.debug("argv0: ['%s'], attrs.getValue(attrName): ['%s']",attrName, attrs.getValue(attrName))
+	log.debug("---input argument---")
+	log.debug("sys.argv[0]: ['%s']",sys.argv[0])
+	log.debug("sys.argv[1]: ['%s']",sys.argv[1])
+	
+
+	aReaderAppEnv = ReaderAppEnv.ReaderAppEnv()
+	aReaderAppEnv.saveAppEnvInfo(CONS.INPUT_APP_ENV_XML)
+	aSqlWriterApp = SqlWriterApp()
+	aSqlWriterApp.writeSqlCore(aReaderAppEnv)
+
+# 	if aReaderAppEnv.appEnvData["isUmlCaseInput"]=='True':
+# 		#deliverableType = 'Class'
+# 		aSqlWriterApp = SqlWriterApp()
+# 		aSqlWriterApp.writeSqlCore(aReaderAppEnv)
+		
