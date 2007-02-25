58d7349d5056c15bb7f962e9d099292e19a492fd
kusung25 <kusung25@40af2166-402a-0410-87ae-1d88bcb22c19>
2007-02-25 08:15:37 +0000

    최초등록

---
+++ source/codegen/caseTool/erwin2Input/script/ExcelHelper.py
@@ -0,0 +1,150 @@
+﻿# -*- coding: utf-8 -*-
+import sys
+from win32com.client import Dispatch
+import pythoncom
+from CommonUtil import *
+#from ReaderAppEnvXml import *
+from Constants import *
+import CommonUtil as comUtil
+
+ComUtil = CommonUtil()
+
+import logging
+import utils
+
+log = logging.getLogger('ExcelHelper')
+
+class ExcelHelper:
+	def __init__(self):
+		self.aModelInfo = None
+
+	def setModelInfo(self, aModelInfo):
+		self.aModelInfo = aModelInfo
+		#self.CONS = CONS
+		
+	def setCons(self, CONS):
+		self.CONS = CONS
+		
+	def getModelInfo(self):
+		return self.aModelInfo
+		
+	def openExcelSheet(self, filename):
+		try:
+			self.xl = Dispatch("Excel.Application")
+			#filename은 풀 패스를 가지고 있어야 함
+			self.xl.Workbooks.Open(filename)
+			self.filename = filename
+			self.workFile = self.xl.ActiveWorkbook.Name
+			
+		except pythoncom.com_error, (hr, msg, exc, arg):
+			ComUtil.printPythonComError(hr, msg, exc, arg)
+			
+	def getSheet(self, inName):
+		try:
+			xl = self.xl; workFile = self.workFile
+
+			cnt = xl.Workbooks(workFile).Sheets.Count
+			
+			for x in range(1,cnt):
+				if xl.Sheets(x).Name == inName:
+					aTargetSheet = xl.Workbooks(workFile).Sheets(x) # 복사된 시트
+					return aTargetSheet
+		except pythoncom.com_error, (hr, msg, exc, arg):
+			ComUtil.printPythonComError(hr, msg, exc, arg)
+
+	def addSheet(self, templName="templ", sheetName=None):
+		try:
+			xl = self.xl; workFile = self.workFile
+			
+			self.setVisible(False)
+			cnt = xl.Workbooks(workFile).Sheets.Count
+			aSheet = xl.Workbooks(workFile).Sheets(cnt)
+			xl.Workbooks(workFile).Sheets(templName).Copy(aSheet)
+			
+			aTargetSheet = xl.Workbooks(workFile).Sheets(cnt) # 복사된 시트
+			#xl.Sheets(cnt).Name = aClassInfo.className
+			if sheetName == None:
+				xl.Sheets(cnt).Name = cnt - 1
+			else:
+				xl.Sheets(cnt).Name = sheetName
+			self.setVisible(True)
+			
+			return aTargetSheet
+			
+		except pythoncom.com_error, (hr, msg, exc, arg):
+			ComUtil.printPythonComError(hr, msg, exc, arg)
+			
+			
+		
+	def deleteSheet(self, inSheetNameList):
+		try:
+			xl = self.xl
+			xl.DisplayAlerts = False
+			self.setVisible(True)
+			
+			for ws in xl.Worksheets:
+				#inSheetName = ['templ','method_templ']
+				if ws.Name in inSheetNameList:
+					xl.Worksheets(ws.Index).Delete() #Sheets(strSheetName).Index
+			xl.DisplayAlerts = True
+			
+		except pythoncom.com_error, (hr, msg, exc, arg):
+			ComUtil.printPythonComError(hr, msg, exc, arg)
+			
+	def setCurrentRow(self, curRow):
+		self.currentRow = curRow
+
+	def getCurrentRow(self):
+		return self.currentRow
+
+	def setVisible(self, isVisible):
+		#pass
+		self.xl.Visible = isVisible
+
+	#ToDo 로우에 대한 Fit기능을 사용한다.
+	def setRowAutoFit(self, isVisible):
+		pass
+##    Rows("6:12").Select
+##    Rows("6:12").EntireRow.AutoFit
+		
+	def closeExcel(self):
+		try:
+			self.xl.ActiveWorkbook.Close(1) #1이면 저장을 한다.
+			del self.xl
+		except pythoncom.com_error, (hr, msg, exc, arg):
+			ComUtil.printPythonComError(hr, msg, exc, arg)
+			
+	def copyMethodTitle(self, aTargetSheet, inTargetRow):
+		try:
+			xl = self.xl
+			targetSheetName = aTargetSheet.Name
+			targetRow = str(inTargetRow) + ":" + str(inTargetRow)
+
+			self.xl.Visible = True
+			methodTemplSheet='method_templ'
+			xl.Workbooks(self.workFile).Sheets(methodTemplSheet).Select()	##    Sheets("templ").Select
+			xl.Worksheets(methodTemplSheet).Activate()
+			xl.Worksheets(methodTemplSheet).Rows("1:1").Select() ##    Rows("1:1").Select
+
+			xl.Selection.Copy()	##    Selection.Copy
+			xl.Workbooks(self.workFile).Sheets(targetSheetName).Select()	##    Sheets("templ").Select
+			xl.Worksheets(targetSheetName).Rows(targetRow).Select()	##    Rows("7:7").Select
+
+			xl.Worksheets(targetSheetName).Paste()	##    ActiveSheet.Paste
+			
+			self.setCurrentRow(self.getCurrentRow()+1)
+
+		except pythoncom.com_error, (hr, msg, exc, arg):
+			ComUtil.printPythonComError(hr, msg, exc, arg)
+			
+	def writeHeadInfo(self, sh, aClassInfo):
+		# row, col
+		aReaderAppEnv=self.aModelInfo.getReaderAppEnv()
+		sh.Cells(2, 4).Value = aReaderAppEnv.appEnvData["writer"]	# 작성자
+		sh.Cells(2, 6).Value = aReaderAppEnv.appEnvData["writeDate"]# 작성일
+		sh.Cells(3, 6).Value = aReaderAppEnv.appEnvData["subSystemName"] # 서브시스템
+
+
+
+			
+
---
+++ source/codegen/caseTool/erwin2Input/script/ExcelHelperClassList.py
@@ -0,0 +1,59 @@
+﻿# -*- coding: utf-8 -*-
+from ExcelHelper import *
+#-------------------------------------------------------------------------------
+class ExcelHelperClassList(ExcelHelper):
+	def __init__(self, CONS):
+		ExcelHelper.__init__
+		self.setCons(CONS)
+		i = self.CONS.ATTRIBUTE_LIST_START_POSITION
+		self.setCurrentRow(i)
+
+	def writeExcelCoreClassList(self,aExcelHelper, outputfile):
+		aModelInfo = self.aModelInfo
+
+		aExcelHelper.openExcelSheet(outputfile)
+		aExcelHelper.deleteSheet(['input']) #templ
+
+		aTargetSheet = aExcelHelper.addSheet('templ', 'input')
+		#aTargetSheet = aExcelHelper.getSheet('input')
+		
+		outList = aModelInfo.getClassListOrInterfaceList()
+		if not outList:
+			log.debug("---def writeExcelCoreClassList---")
+			log.debug("data is none")
+# 			aExcelHelper.closeExcel()
+# 			sys.exit(0)
+
+		for aClassInfo in outList:
+			self.writeAttribute(aTargetSheet, aClassInfo)
+
+#		aExcelHelper.deleteSheet(['templ']) #templ
+		#aExcelHelper.closeExcel()
+
+#-------------------------------------------------------------------------------
+	def writeAttribute(self, sh, aClassInfo):
+
+		#attributes = self.aModelInfo.getAttributes(aClassInfo)
+		attributes = aClassInfo.fieldList
+		
+		i = self.getCurrentRow()
+		for attr in attributes:
+			self.writeAttributeRecord(sh, i, aClassInfo, attr)
+			i = i + 1
+
+##		#속성정보가 없는 경우에 대한 처리
+##		if i == self.CONS.ATTRIBUTE_LIST_START_POSITION:
+##			self.writeAttributeRecordBlank(sh, i)
+##			i = i + 1
+
+		self.setCurrentRow(i)
+
+	def writeAttributeRecord(self, sh, row, aClassInfo, attr):
+        # NO	ClassName	가시성	타입	속성명
+
+		sh.Cells(row, 1).Value = row - (self.CONS.ATTRIBUTE_LIST_START_POSITION-1)	# 번호
+		sh.Cells(row, 2).Value = aClassInfo.name
+		sh.Cells(row, 3).Value = attr.visibility	# 가시성
+		sh.Cells(row, 4).Value = attr.javaType		# 타입
+		sh.Cells(row, 5).Value = attr.name			# 속성명
+		
